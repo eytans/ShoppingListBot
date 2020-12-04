1f44840f23035c1ffbcac99309e7c492914667c5
Eytan Singher <eytan.singher@gmail.com>
2020-12-04 21:33:01 +0200

    Public Repo: fix bad symbol naming

--- shopping_bot.py
+++ shopping_bot.py
@@ -13,13 +13,13 @@ if len(sys.argv) < 2:
     print("bad arguments. need working folder. exiting")
     exit(1)
 
-workingDir = sys.argv[1]
-os.chdir(workingDir)
+working_dir = sys.argv[1]
+os.chdir(working_dir)
 
 GENERAL_DATA = "main.data"
 HELP = 'help'
 ERROR_LOG = 'errors.log'
-error_log = logging.basicConfig(filename=ERROR_LOG, level=logging.ERROR)
+logging.basicConfig(filename=ERROR_LOG, level=logging.ERROR)
 
 ADD_RESPONSE = 'What item do you want to add to the list?'
 REMOVE_RESPONSE = 'What item do you want to remove from the list?'
@@ -34,11 +34,12 @@ LIST_MAX_SIZE = 300
 ITEM_MAX_SIZE = 100
 BACKUP_LIST_NAME = 'list.bak'
 
-helpText = None
-shoppingLists = {}
+help_text = None
+shopping_lists = {}
 force = {'force_reply': True}
 
-def readData(data_path):
+
+def read_data(data_path):
     data = {}
     if os.path.exists(data_path):
         with open(data_path, 'r') as dataFile:
@@ -51,19 +52,19 @@ def readData(data_path):
     return data
 
 
-def updateData(data_path, data):
+def update_data(data_path, data):
     if not os.path.exists(os.path.abspath(os.path.join(data_path, os.pardir))):
-        os.mkdirs(os.path.abspath(os.path.join(data_path, os.pardir)))
+        os.makedirs(os.path.abspath(os.path.join(data_path, os.pardir)))
     with open(data_path, 'w') as data_file:
         for key, val in data.items():
             data_file.write(str(key) + '=' + str(val) + '\n')
 
 
 def getsettingspath(chat_id):
-    return getlistpath(chat_id).replace('list', 'settings')
+    return get_listpath(chat_id).replace('list', 'settings')
 
 
-def getlistpath(chat_id, list_name=None):
+def get_listpath(chat_id, list_name=None):
     if not os.path.exists(str(chat_id)):
         os.makedirs(str(chat_id))
     if list_name is None:
@@ -72,10 +73,10 @@ def getlistpath(chat_id, list_name=None):
 
 
 def getlist(chat_id, list_name=None):
-    global shoppingLists
-    if chat_id in shoppingLists:
-        return shoppingLists[chat_id]
-    list_path = getlistpath(chat_id, list_name)
+    global shopping_lists
+    if chat_id in shopping_lists:
+        return shopping_lists[chat_id]
+    list_path = get_listpath(chat_id, list_name)
     if not os.path.exists(str(list_path)):
         return []
     with open(list_path, 'r') as list_file:
@@ -85,11 +86,11 @@ def getlist(chat_id, list_name=None):
 
 
 def updatelist(chat_id, new_list):
-    global shoppingLists
+    global shopping_lists
     new_list = [item.replace(os.linesep, '').replace('\r', '').replace('\n', '').strip() for item in new_list]
     new_list = [item for item in new_list if item != '']
-    shoppingLists[chat_id] = new_list
-    with open(getlistpath(chat_id), 'w') as list_file:
+    shopping_lists[chat_id] = new_list
+    with open(get_listpath(chat_id), 'w') as list_file:
         for item in new_list:
             list_file.write(item + '\n')
 
@@ -98,7 +99,7 @@ def sendmessage(chat_id, reply, reply_to=None, force=None):
     if reply == '':
         return
     try:
-        bot.sendMessage(chat_id=chat_id, text=reply,  reply_to_message_id=reply_to, reply_markup=force)
+        bot.sendMessage(chat_id=chat_id, text=reply, reply_to_message_id=reply_to, reply_markup=force)
     except:
         print(traceback.format_exc())
         if error_log is not None:
@@ -117,16 +118,18 @@ def getitems(text, command=None):
 
 
 commands = None
+
+
 def getCommands():
     global commands
-    global helpText
+    global help_text
     if commands is not None:
         return commands
 
-    if helpText is None:
+    if help_text is None:
         with open(HELP, 'r') as helpFile:
-            helpText = "\n".join(helpFile.readlines())
-    lines = helpText.splitlines()
+            help_text = "\n".join(helpFile.readlines())
+    lines = help_text.splitlines()
     res = []
     for line in lines:
         if '-' not in line:
@@ -137,6 +140,8 @@ def getCommands():
 
 
 pattern = None
+
+
 def getcleanitems(items):
     global pattern
     commands = getCommands()
@@ -151,15 +156,15 @@ def getcleanitems(items):
 
 
 # returns the answer string
-def handleHelp(text, chat_id, message_id):
-    global helpText
-    if helpText is None:
+def handle_help(text, chat_id, message_id):
+    global help_text
+    if help_text is None:
         with open(HELP, 'r') as helpFile:
-            helpText = "\n".join(helpFile.readlines())
-    sendmessage(chat_id, helpText)
+            help_text = "\n".join(helpFile.readlines())
+    sendmessage(chat_id, help_text)
 
 
-def addItemsToList(chat_id, items):
+def add_items(chat_id, items):
     orig_items_len = len(items)
     current = getlist(chat_id)
     if (len(current) + len(items)) > LIST_MAX_SIZE:
@@ -176,21 +181,21 @@ def addItemsToList(chat_id, items):
     sendmessage(chat_id, reply)
 
 
-def handleAdd(text, chat_id, message_id):
+def handle_add(text, chat_id, message_id):
     global force
     items = getcleanitems(getitems(text, '/add'))
     if len(getitems(text, '/add')) == 0:
         sendmessage(chat_id, ADD_RESPONSE, message_id, force)
         return
-    addItemsToList(chat_id, items)
+    add_items(chat_id, items)
 
 
-def handleAddResponse(text, chat_id, message_id):
+def handle_add_response(text, chat_id, message_id):
     items = getcleanitems(getitems(text, '/add'))
-    addItemsToList(chat_id, items)
+    add_items(chat_id, items)
 
 
-def removeItemsFromList(items, chat_id):
+def remove_items(items, chat_id):
     new_list = getlist(chat_id)
     count = 0
     for item in items:
@@ -202,24 +207,24 @@ def removeItemsFromList(items, chat_id):
     sendmessage(chat_id, 'removed ' + str(count) + ' items')
 
 
-def handleRemove(text, chat_id, message_id):
+def handle_remove(text, chat_id, message_id):
     global force
     items = getitems(text, '/remove')
     if len(items) == 0:
         sendmessage(chat_id, REMOVE_RESPONSE, message_id, force)
         return
-    removeItemsFromList(items, chat_id)
+    remove_items(items, chat_id)
 
 
-def handleRemoveResponse(text, chat_id, message_id):
+def handle_remove_response(text, chat_id, message_id):
     global force
     items = getitems(text, '/remove')
-    removeItemsFromList(items, chat_id)
+    remove_items(items, chat_id)
 
 
-def handleShowlist(text, chat_id, message_id):
+def handle_showlist(text, chat_id, message_id):
     settings_path = getsettingspath(chat_id)
-    settings = readData(settings_path)
+    settings = read_data(settings_path)
     sort_list = True
     if SORT_LIST_KEY in settings:
         sort_list = bool(settings[SORT_LIST_KEY])
@@ -244,25 +249,25 @@ def handleShowlist(text, chat_id, message_id):
     sendmessage(chat_id, reply)
 
 
-def handleSettings(text, chat_id, message_id):
+def handle_settings(text, chat_id, message_id):
     global force
     if (len(text.split()) == 1) or ((text.split()[1] != 'a') and (text.split()[1] != 'i')):
         sendmessage(chat_id, SETTINGS_RESPONSE, message_id, force)
         return
-    order = text.split()[1] # note that i use this expression in if
+    order = text.split()[1]  # note that i use this expression in if
     settings_path = getsettingspath(chat_id)
     settings = {}
     settings[SORT_LIST_KEY] = (order == 'a')
-    updateData(settings_path, settings)
+    update_data(settings_path, settings)
     sendmessage(chat_id, 'ok')
 
 
-def handleClear(text, chat_id, message_id):
+def handle_clear(text, chat_id, message_id):
     updatelist(chat_id, [])
     sendmessage(chat_id, 'ok')
 
 
-def getReplyBeginingByText(text):
+def get_reply_begining_by_text(text):
     if text == ADD_RESPONSE:
         return '/add '
     elif text == REMOVE_RESPONSE:
@@ -275,8 +280,8 @@ def getReplyBeginingByText(text):
         return ''
 
 
-def handleMessage(msg):
-    global shoppingLists
+def handle_message(msg):
+    global shopping_lists
     global error_log
     global force
     if msg is None:
@@ -291,19 +296,19 @@ def handleMessage(msg):
     if 'group_chat_created' in msg and msg['group_chat_created'] == 'True':
         sendmessage(chat_id, 'Hi, im here to help you shop. if you want more info just write /help')
     if is_reply and reply_text.startswith(ADD_RESPONSE):
-        handleAddResponse(text, chat_id, msg_id)
-        if os.path.exists(getlistpath(chat_id, BACKUP_LIST_NAME)):
-            os.remove(getlistpath(chat_id, BACKUP_LIST_NAME))
+        handle_add_response(text, chat_id, msg_id)
+        if os.path.exists(get_listpath(chat_id, BACKUP_LIST_NAME)):
+            os.remove(get_listpath(chat_id, BACKUP_LIST_NAME))
     elif is_reply and reply_text.startswith(REMOVE_RESPONSE):
-        handleRemoveResponse(text, chat_id, msg_id)
-        if os.path.exists(getlistpath(chat_id, BACKUP_LIST_NAME)):
-            os.remove(getlistpath(chat_id, BACKUP_LIST_NAME))
+        handle_remove_response(text, chat_id, msg_id)
+        if os.path.exists(get_listpath(chat_id, BACKUP_LIST_NAME)):
+            os.remove(get_listpath(chat_id, BACKUP_LIST_NAME))
     elif is_reply and reply_text.startswith(SETTINGS_RESPONSE):
-        handleSettings(text, chat_id, msg_id)
+        handle_settings(text, chat_id, msg_id)
     elif is_reply and reply_text.startswith(EDIT_RESPONSE):
         new_list = getcleanitems(getitems(text, None))
-        if os.path.exists(getlistpath(chat_id)):
-            shutil.copy2(getlistpath(chat_id), getlistpath(chat_id, BACKUP_LIST_NAME))
+        if os.path.exists(get_listpath(chat_id)):
+            shutil.copy2(get_listpath(chat_id), get_listpath(chat_id, BACKUP_LIST_NAME))
         reply = 'ok'
         if len(new_list) > LIST_MAX_SIZE:
             new_list = new_list[:LIST_MAX_SIZE]
@@ -316,40 +321,41 @@ def handleMessage(msg):
         updatelist(chat_id, new_list)
         sendmessage(chat_id, reply)
     elif text.startswith('/help'):
-        handleHelp(text, chat_id, msg_id)
+        handle_help(text, chat_id, msg_id)
     elif text.startswith('/add'):
         text = text.replace('/add@eytans_shopping_bot', '/add')
-        handleAdd(text, chat_id, msg_id)
-        if os.path.exists(getlistpath(chat_id, BACKUP_LIST_NAME)):
-            os.remove(getlistpath(chat_id, BACKUP_LIST_NAME))
+        handle_add(text, chat_id, msg_id)
+        if os.path.exists(get_listpath(chat_id, BACKUP_LIST_NAME)):
+            os.remove(get_listpath(chat_id, BACKUP_LIST_NAME))
     elif text.startswith('/remove'):
         text = text.replace('/remove@eytans_shopping_bot', '/remove')
-        handleRemove(text, chat_id, msg_id)
-        if os.path.exists(getlistpath(chat_id, BACKUP_LIST_NAME)):
-            os.remove(getlistpath(chat_id, BACKUP_LIST_NAME))
+        handle_remove(text, chat_id, msg_id)
+        if os.path.exists(get_listpath(chat_id, BACKUP_LIST_NAME)):
+            os.remove(get_listpath(chat_id, BACKUP_LIST_NAME))
     elif text.startswith('/showlist'):
-        handleShowlist(text, chat_id, msg_id)
+        handle_showlist(text, chat_id, msg_id)
     elif text.startswith('/settings'):
-        handleSettings(text, chat_id, msg_id)
+        handle_settings(text, chat_id, msg_id)
     elif text.startswith('/clear'):
-        if os.path.exists(getlistpath(chat_id)):
-            shutil.copy2(getlistpath(chat_id), getlistpath(chat_id, BACKUP_LIST_NAME))
-        handleClear(text, chat_id, msg_id)
+        if os.path.exists(get_listpath(chat_id)):
+            shutil.copy2(get_listpath(chat_id), get_listpath(chat_id, BACKUP_LIST_NAME))
+        handle_clear(text, chat_id, msg_id)
     elif text.startswith('/edit'):
         text = '/showlist'
-        handleShowlist(text, chat_id, msg_id)
+        handle_showlist(text, chat_id, msg_id)
         sendmessage(chat_id, EDIT_RESPONSE, msg_id, force)
     elif text.startswith('/undoedit'):
-        if not os.path.exists(getlistpath(chat_id, BACKUP_LIST_NAME)):
-            sendmessage(chat_id, 'Sorry, i dont have a backup. if you didnt edit or used add/remove i wouldnt have one..')
+        if not os.path.exists(get_listpath(chat_id, BACKUP_LIST_NAME)):
+            sendmessage(chat_id,
+                        'Sorry, i dont have a backup. if you didnt edit or used add/remove i wouldnt have one..')
         else:
-            shutil.copy2(getlistpath(chat_id, BACKUP_LIST_NAME), getlistpath(chat_id))
-            os.remove(getlistpath(chat_id, BACKUP_LIST_NAME))
-            del shoppingLists[chat_id]
+            shutil.copy2(get_listpath(chat_id, BACKUP_LIST_NAME), get_listpath(chat_id))
+            os.remove(get_listpath(chat_id, BACKUP_LIST_NAME))
+            del shopping_lists[chat_id]
             sendmessage(chat_id, 'ok')
 
 
-data = readData(GENERAL_DATA)
+data = read_data(GENERAL_DATA)
 
 if SHOPPING_BOT_TOKEN_KEY not in data:
     print('error: no bot token. cant be a bot without a bot')
@@ -357,5 +363,4 @@ if SHOPPING_BOT_TOKEN_KEY not in data:
 shopping_bot_token = data[SHOPPING_BOT_TOKEN_KEY].strip()
 bot = telepot.Bot(token=shopping_bot_token)
 
-bot.message_loop(handleMessage, run_forever=True)
-
+bot.message_loop(handle_message, run_forever=True)
